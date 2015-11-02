b9d1f867460b414e7c92c061dbb9bb4e5266629b
d228m3286w <d228m3286w@gmail.com>
2015-11-02 10:32:13 -0700

    post_blog working

--- routes/route.js
+++ routes/route.js
@@ -1,17 +1,2 @@
 
 
-// var router = express.Router();
-// var mongoose = require('mongoose');
-// var bodyParser = require('body-parser');
-// router.use(bodyParser.urlencoded({ extended: true }))
-
-// var validBlogs = [];
-
-// function filterByTitle(obj) {
-//  if ('title' in obj && typeof(obj.title) === 'string') {
-//    validBlogs.push(obj);
-//    return true;
-//  } else {
-//    return false;
-//  }
-// };
