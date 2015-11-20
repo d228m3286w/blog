e6bf148c5f3f312bf2fcb65ff50183135bdf3f4c
d228m3286w <d228m3286w@gmail.com>
2015-11-20 13:29:32 -0700

    just a test

---
+++ client/blogComment.js
@@ -0,0 +1,38 @@
+var React = require('react');
+
+var CommentForm = React.createClass({
+	 handleCommentSubmit: function(e) {
+    e.preventDefault();
+
+    var body = React.findDOMNode(this.refs.body).value.trim();
+    var data = ({body: body});
+     	$.ajax({
+            url: "/api/blog/" + this.props.blogId + "/comment",
+            dataType: 'json',
+            data: data,
+            type: 'POST',
+                success:function(response){
+                	console.log("Posting comments", response)
+                	document.location="/blog.html"
+                }.bind(this),
+                error: function(xhr,status, err){
+                    console.log("NOT POSTING DATA")
+                    console.log(data)
+                    console.error(this.props.url, status, err.toString());
+                }.bind(this)
+        })
+    },
+
+render: function() {
+	return(
+		<div>
+  			<form method="POST">
+  				<textarea type="text" ref="body" className="form-control" id="commentField" placeholder="Comments About How Awesome I Am"/>
+				<button onClick={this.handleCommentSubmit} type="submit" id="commentButton" className="btn btn-primary">Leave Comment</button>
+  			</form>
+ 		</div>
+  )
+}
+});
+module.exports = CommentForm;
+   
--- client/blogList.js
+++ client/blogList.js
@@ -1,32 +1,9 @@
 var React = require('react');
+var CommentForm = require('./blogComment');
+
 
 var BlogList = React.createClass({
-
-    handleCommentSubmit: function(e) {
-    e.preventDefault();
-    console.log(blogId)
-        var body = React.findDOMNode(this.refs.body).value.trim();
-
-            var data = ({body: body});
-           
-            $.ajax({
-                url: "/api/blog/" + this.props.blogId + "/comment",
-                dataType: 'json',
-                data: data,
-                type: 'POST',
-                    success:function(response){
-                    console.log("Posting comments", data, response)
-                    }.bind(this),
-                    error: function(xhr,status, err){
-                        console.log("NOT POSTING DATA")
-                        console.log(data)
-                        console.error(this.props.url, status, err.toString());
-                    }.bind(this)
-            })
-    },
-
     render: function() {
-        var self = this;
     	var blogdata = this.props.data.map(function(blogs){
             if(blogs.comments) {
                 var blogComments = blogs.comments.map(function(i){
@@ -43,10 +20,7 @@ var BlogList = React.createClass({
     				<div>
     					<h4> {blogs.title} </h4>
     					<article> {blogs.body} <p id="peepsComments"> {blogComments} </p> </article>
-                        <form method="POST">
-                            <textarea type="text" ref="body" className="form-control" id="commentField" placeholder="Comments About How Awesome I Am"/>
-                            <button onClick={self.handleCommentSubmit}  blogId={blogs._id} type="submit" id="commentButton" className="btn btn-primary">Leave Comment</button>
-                        </form>
+                         <CommentForm blogId={blogs._id}/>
     				</div>
     			)
 
